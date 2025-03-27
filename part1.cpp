14c37f4a8f6e790dda45f9b26d3e001380d5c714
amayalaing <147103436+amayalaing@users.noreply.github.com>
2025-03-27 16:06:46 -0400

    Updated part1.cpp - amaya laing

--- part1.cpp
+++ part1.cpp
@@ -14,6 +14,21 @@ Note: All sides are integers.
 
 int main() {
     cout<<"Part 1"<<endl; //do not remove, edit, or change the placement of this line.
-    
+    int numOfSides=0;//the number of sides
+    string shape;//the shape from the user
+    int lengthOfSide;//length of each side
+    int Perimeter=0;//then calculate the 
+    int i;
+
+    cout << "HELLO! Please enter the number of sides:" << endl;
+    cin >> numOfSides;
+    cout << "Please enter the shape:" << endl;
+    cin >> shape;
+    for(i=1;i <= numOfSides; i++){
+    cout << "Please enter the lengths of each side:" << endl;
+    cin >> lengthOfSide;
+    Perimeter += lengthOfSide;
+    }
+    cout << "The perimeter of the " << shape << " is: " <<     Perimeter << endl;
     return 0;
  }
