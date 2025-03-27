4e9ae1035685e2b7b2abb57ede4f1f0d06307fe7
amayalaing <147103436+amayalaing@users.noreply.github.com>
2025-03-27 16:33:21 -0400

    Updated part2.cpp - amaya laing

--- part2.cpp
+++ part2.cpp
@@ -10,11 +10,56 @@ Note: All sides are not necessarily same the length.
 Note: All sides are integers.
 */
 
-#include <iostream>
+ #include <iostream>
 
-using namespace std;
+ using namespace std;
+
+class Shape {
+    private:
+        int numOfSides;
+        int* sideLengths;
+
+    public:
+        Shape(int sides) {
+            numOfSides = sides;
+            sideLengths = new int[numOfSides];
+        }
+
+        void setSideLength(int index, int length) {
+            sideLengths[index] = length;
+        }
+
+        int calculatePerimeter() {
+            int perimeter = 0;
+            for (int i = 0; i < numOfSides; i++) {
+                perimeter += sideLengths[i];
+            }
+            return perimeter;
+        }
+};
 
 int main() {
-    cout<<"Part 2"<<endl; //do not remove, edit, or change the placement of this line.
+    cout << "Part 1" << endl; //do not remove, edit, or change the placement of this line.
+    int numOfSides = 0; //the number of sides
+    string shape; //the shape from the user
+    int lengthOfSide; //length of each side
+    int Perimeter = 0; //then calculate the perimeter
+
+    cout << "HELLO! Please enter the number of sides:" << endl;
+    cin >> numOfSides;
+    cout << "Please enter the shape:" << endl;
+    cin >> shape;
+
+    Shape s(numOfSides);
+
+    for (int i = 0; i < numOfSides; i++) {
+        cout << "Please enter the length of each side " << endl;
+        cin >> lengthOfSide;
+        s.setSideLength(i, lengthOfSide);
+    }
+
+    Perimeter = s.calculatePerimeter();
+    cout << "The perimeter of the " << shape << " is: " << Perimeter << endl;
+
     return 0;
 }
